2c019e09709133619bddea02b5a2aa2d7ab05d01
vaca22 <wsmslgh@gmail.com>
2022-06-19 22:57:50 +0800

    adsf

--- app/src/main/java/com/vaca/esp32_android_ble/MainActivity.kt
+++ app/src/main/java/com/vaca/esp32_android_ble/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.vaca.esp32_android_ble
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +12,8 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.vaca.esp32_android_ble.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +23,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val requestVoicePermission= registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            Log.e("fuck",it.get(Manifest.permission.ACCESS_FINE_LOCATION).toString())
+
+        }
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            requestVoicePermission.launch( arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+
+
+        }
+
+
+
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -35,15 +60,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
---
+++ app/src/main/java/com/vaca/esp32_android_ble/bean/HavePermission.kt
@@ -0,0 +1,3 @@
+package com.vaca.esp32_android_ble.bean
+
+data class HavePermission(val havePer:Boolean)
