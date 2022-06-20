43b4fdd8234fdd09b0dd09350bb099de63bd6596
vaca22 <wsmslgh@gmail.com>
2022-06-20 20:04:03 +0800

    adsfasdfsdfsdfsadfghffdsasdfgd发vsdf大哥fgfdgdfgasd的萨芬sdfrgsdfg

--- app/src/main/java/com/vaca/esp32_android_ble/fragment/SecondFragment.kt
+++ app/src/main/java/com/vaca/esp32_android_ble/fragment/SecondFragment.kt
@@ -1,6 +1,7 @@
 package com.vaca.esp32_android_ble.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -28,7 +29,9 @@ class SecondFragment : Fragment() {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
 
 
-
+        binding.upload.setOnClickListener {
+            Log.e("good","uyes")
+        }
 
 
 
