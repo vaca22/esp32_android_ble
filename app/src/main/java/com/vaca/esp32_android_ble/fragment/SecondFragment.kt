6abf98f31ee40bf102f03c8943073c37a32a699a
vaca22 <wsmslgh@gmail.com>
2022-06-20 19:55:41 +0800

    adsfasdfsdfsdfsadfghffdsasdfgd发vsdf大哥fgfdgdfgasd

--- app/src/main/java/com/vaca/esp32_android_ble/fragment/SecondFragment.kt
+++ app/src/main/java/com/vaca/esp32_android_ble/fragment/SecondFragment.kt
@@ -23,9 +23,14 @@ class SecondFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
+
+
+
+
         return binding.root
 
     }
@@ -33,9 +38,7 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+
     }
 
     override fun onDestroyView() {
