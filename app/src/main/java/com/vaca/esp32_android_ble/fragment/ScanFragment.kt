6750b830e32e4511e72666034fdfdfe0f94e0f8f
vaca22 <wsmslgh@gmail.com>
2022-07-16 11:20:41 +0800

    dsfg5252dsfdsffdasasddasssdaf

--- app/src/main/java/com/vaca/esp32_android_ble/fragment/ScanFragment.kt
+++ app/src/main/java/com/vaca/esp32_android_ble/fragment/ScanFragment.kt
@@ -2,10 +2,14 @@ package com.vaca.esp32_android_ble.fragment
 
 import android.bluetooth.BluetoothDevice
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -36,8 +40,27 @@ class ScanFragment : Fragment(), BleViewAdapter.ItemClickListener,   BleScanMana
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
-
+    lateinit var myInflater: LayoutInflater
+    fun bindSet2(boolean: Boolean) {
+        Toast(requireContext()).apply {
+            val layout = myInflater.inflate(R.layout.toast_bind_layout, null)
+            if (boolean) {
+                layout.findViewById<TextView>(R.id.dada).apply {
+                    text = "开启成功"
+                }
+                layout.findViewById<ImageView>(R.id.gr).setImageResource(R.drawable.success_icon)
+            } else {
+                layout.findViewById<TextView>(R.id.dada).apply {
+                    text = "添加失败， 已有此成员"
+                }
+                layout.findViewById<ImageView>(R.id.gr).setImageResource(R.drawable.failure_icon)
+            }
+            setGravity(Gravity.CENTER, 0, 0)
+            duration = Toast.LENGTH_SHORT
+            setView(layout)
+            show()
+        }
+    }
 
 
     override fun onCreateView(
@@ -51,6 +74,7 @@ class ScanFragment : Fragment(), BleViewAdapter.ItemClickListener,   BleScanMana
             }
         }
 
+         myInflater=inflater
         _binding =FragmentScanBinding.inflate(inflater, container, false)
 
 
@@ -65,6 +89,14 @@ class ScanFragment : Fragment(), BleViewAdapter.ItemClickListener,   BleScanMana
             //binding.state.text=it
         }
 
+
+        _binding!!.start.setOnClickListener {
+            bindSet2(true)
+            scan.start()
+        }
+        _binding!!.stop.setOnClickListener {
+            scan.stop()
+        }
         return binding.root
 
     }
