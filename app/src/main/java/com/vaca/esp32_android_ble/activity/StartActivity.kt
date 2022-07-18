f67d0b9dbcf672f2ae218e415b9faea906a89589
liguanghao <liguanghao@viatomtech.com>
2022-07-18 20:48:20 +0800

    SDGsaasdfsadfgfqewdddsfs

--- app/src/main/java/com/vaca/esp32_android_ble/activity/StartActivity.kt
+++ app/src/main/java/com/vaca/esp32_android_ble/activity/StartActivity.kt
@@ -1,10 +1,19 @@
 package com.vaca.esp32_android_ble.activity
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Window
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.vaca.esp32_android_ble.MainActivity
 
 import com.vaca.esp32_android_ble.databinding.ActivityStartBinding
@@ -19,6 +28,7 @@ class StartActivity : AppCompatActivity() {
 
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestPermission()
 
         binding.aa.setOnClickListener {
             val intent=Intent(this@StartActivity, MainActivity::class.java)
@@ -28,5 +38,101 @@ class StartActivity : AppCompatActivity() {
             this.finish()
         }
 
+    }
+
+    private val permissionRequestCode = 521
+    private fun checkP(p: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, p) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        val ps: Array<String> = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (!checkP(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, ps, permissionRequestCode)
+            return
+        } else {
+            initA()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            permissionRequestCode -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initA()
+            } else {
+                startActivity(Intent(this, MainActivity::class.java))
+                this.finish()
+            }
+            else -> {
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun initA() {
+        if (!isLocationEnabled()) {
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivityForResult(intent, REQUEST_LOCATION)
+        } else {
+            initB()
+        }
+    }
+
+    fun initB() {
+        val bluetoothManager =
+            getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+        if (bluetoothAdapter == null) {
+            val enableBtIntent = Intent(
+                BluetoothAdapter.ACTION_REQUEST_ENABLE
+            )
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            return;
+        }
+        if (!(bluetoothAdapter.isEnabled)) {
+            val enableBtIntent = Intent(
+                BluetoothAdapter.ACTION_REQUEST_ENABLE
+            )
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            return;
+        }
+        startActivity(Intent(this, MainActivity::class.java))
+        this.finish()
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val lm = getSystemService(LOCATION_SERVICE) as LocationManager
+        var gps_enabled = false
+        var network_enabled = false
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        } catch (ex: Exception) {
+        }
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        } catch (ex: Exception) {
+        }
+        return (gps_enabled || network_enabled)
+    }
+
+    private val REQUEST_LOCATION = 223
+    private val REQUEST_ENABLE_BT = 224
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_LOCATION) {
+            initB()
+        } else if (requestCode == REQUEST_ENABLE_BT) {
+            initB()
+        }
     }
 }
