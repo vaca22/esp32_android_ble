6e568d3fc5785bc7b369881768cdcfd16591b229
liguanghao <liguanghao@viatomtech.com>
2022-09-03 23:22:41 +0800

    dfghfdgdsfgfdgcvrtfgyfdgtyhtry

--- app/src/main/java/com/vaca/esp32_android_ble/ble/wt02/blepower/BleCmd.java
+++ app/src/main/java/com/vaca/esp32_android_ble/ble/wt02/blepower/BleCmd.java
@@ -5,7 +5,7 @@ import static com.vaca.esp32_android_ble.ble.wt02.utils.CRCUtils.calCRC8;
 
 public class BleCmd {
 
-    public static int ER2_RESET = 0xE3;
+    public static int ACTIVATE = 0xA1;
     private static int seqNo = 0;
 
 
@@ -17,17 +17,20 @@ public class BleCmd {
     }
 
 
-    public static byte[] reset() {
-        int len = 0;
-        byte[] cmd = new byte[8 + len];
-        cmd[0] = (byte) 0xA5;
-        cmd[1] = (byte) ER2_RESET;
-        cmd[2] = (byte) ~ER2_RESET;
-        cmd[3] = (byte) 0x00;
-        cmd[4] = (byte) seqNo;
-        cmd[5] = (byte) 0;
-        cmd[6] = (byte) 0;
-        cmd[7] = calCRC8(cmd);
+    public static byte[] activate(boolean b) {
+        int len = 1;
+        byte[] cmd = new byte[6 + len];
+        cmd[0] = (byte) 0xCA;
+        cmd[1] = (byte) ACTIVATE;
+        cmd[2] = (byte) ~ACTIVATE;
+        cmd[3] = (byte) seqNo;
+        cmd[4] = (byte) 1;
+        if(b){
+            cmd[5] = (byte) 1;
+        }else{
+            cmd[5] = (byte) 0;
+        }
+        cmd[6] = calCRC8(cmd);
         addNo();
         return cmd;
     }
@@ -35,12 +38,5 @@ public class BleCmd {
 
 
 
-    public static byte[] intToByteArray(int i) {
-        byte[] result = new byte[4];
-        result[3] = (byte) ((i >> 24) & 0xFF);
-        result[2] = (byte) ((i >> 16) & 0xFF);
-        result[1] = (byte) ((i >> 8) & 0xFF);
-        result[0] = (byte) (i & 0xFF);
-        return result;
-    }
+
 }
