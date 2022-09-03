351dc9a0348c589f65f128a3ae81214efea95180
liguanghao <liguanghao@viatomtech.com>
2022-09-03 22:44:49 +0800

    fdsgfdasd

--- app/src/main/java/com/vaca/esp32_android_ble/MainActivity.kt
+++ app/src/main/java/com/vaca/esp32_android_ble/MainActivity.kt
@@ -3,6 +3,11 @@ package com.vaca.esp32_android_ble
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.IBinder
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.activity.result.contract.ActivityResultContracts
@@ -16,6 +21,51 @@ import com.vaca.esp32_android_ble.fragment.ScanFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    override fun dispatchTouchEvent(me: MotionEvent): Boolean {
+        if (me.action == MotionEvent.ACTION_DOWN) {  //把操作放在用户点击的时候
+            val v = currentFocus //得到当前页面的焦点,ps:有输入框的页面焦点一般会被输入框占据
+            if (isShouldHideKeyboard(v, me)) { //判断用户点击的是否是输入框以外的区域
+                hideKeyboard(v!!.windowToken) //收起键盘
+            }
+        }
+        return super.dispatchTouchEvent(me)
+    }
+
+
+    private fun isShouldHideKeyboard(v: View?, event: MotionEvent): Boolean {
+        if (v != null && v is EditText) {  //判断得到的焦点控件是否包含EditText
+            val l = intArrayOf(0, 0)
+            v.getLocationInWindow(l)
+            val left = l[0]
+            //得到输入框在屏幕中上下左右的位置
+            val top = l[1]
+            val bottom = top + v.getHeight()
+            val right = left + v.getWidth()
+            return if (event.x > left && event.x < right && event.y > top && event.y < bottom) {
+                // 点击位置如果是EditText的区域，忽略它，不收起键盘。
+                false
+            } else {
+                true
+            }
+        }
+        // 如果焦点不是EditText则忽略
+        return false
+    }
+
+
+    /**
+     * 获取InputMethodManager，隐藏软键盘
+     * @param token
+     */
+    private fun hideKeyboard(token: IBinder?) {
+        if (token != null) {
+            val im: InputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
