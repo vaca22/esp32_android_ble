807bf22eb51f984e14f405b38dfadbe6f06547ed
liguanghao <liguanghao@viatomtech.com>
2022-09-07 21:24:10 +0800

    dfhfghxcvgedrftgfhdsfgasdfasdfsd

--- app/src/main/java/com/vaca/esp32_android_ble/ble/wt02/blepower/NotifyListener.kt
+++ app/src/main/java/com/vaca/esp32_android_ble/ble/wt02/blepower/NotifyListener.kt
@@ -3,11 +3,7 @@ package com.vaca.esp32_android_ble.ble.wt02.blepower
 import android.bluetooth.BluetoothDevice
 import no.nordicsemi.android.ble.data.Data
 
-/**
- * author: wujuan
- * created on: 2021/1/21 14:42
- * description:
- */
+
 internal interface NotifyListener {
 
     fun onNotify(device: BluetoothDevice, data: Data)
